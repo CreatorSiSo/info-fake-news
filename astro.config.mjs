e5f8022275b004589bd4e6e30ad22fb071256b9c
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2022-01-08 21:43:14 +0100

    Add prettier.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('astro').AstroUserConfig} */
 export default {
-  pages: "./src/pages",
-  dist: "./build",
-  public: "./public",
+  pages: './src/pages',
+  dist: './build',
+  public: './public',
   buildOptions: {
     // Your public domain used to generate sitemaps and canonical URLs.
-    site: "https://creatorsiso.github.io/info-fake-news",
+    site: 'https://info-fake-news.vercel.app',
     sitemap: true,
   },
   devOptions: {
-    hostname: "0.0.0.0",
+    hostname: '0.0.0.0',
     port: 3000,
   },
   renderers: [],
