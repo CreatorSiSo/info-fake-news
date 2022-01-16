e63796b6bf783113dbeab715aff9917f5aa20597
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2022-01-16 06:19:14 +0100

    Update

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neutral-950': '#101010',
+      },
+    },
+  },
+  plugins: [],
+};
